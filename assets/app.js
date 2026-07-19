8bea47503b6b9bd68db658f92a73b082d815a9af
Ghost404az <chess2026z@proton.me>
2026-07-19 03:08:20 -0400

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,9 @@
 console.log("Script loaded!");
-document.body.style.backgroundColor = "#f0f0f0"; // تغيير لون الخلفية للتأكد أن الملف يعمل
+document.body.style.backgroundColor = "#f0f0f0"; 
 
 const contentArea = document.getElementById('content-area');
 if (contentArea) {
-    contentArea.innerHTML = "تم الاتصال بنجاح. النظام يعمل بكامل طاقته.";
+    contentArea.innerHTML = "Working.";
     console.log("Content updated successfully.");
 } else {
     console.log("Could not find content-area element!");
